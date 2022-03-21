2fc8e83476edc901d369bf1a672d0466f835586d
Andrei Iacob <77151602+IacobAndrei15@users.noreply.github.com>
2022-03-21 19:39:10 +0200

    jackson

--- Lab 5/src/main/java/Compulsory.java
+++ Lab 5/src/main/java/Compulsory.java
@@ -2,8 +2,20 @@ public class Compulsory {
 
     public static void main(String[] args) {
 
-        Book book1 = new Book("knuth67", "The Art of Computer Programming", "d:/books/programming/tacp.ps", 1967, "Donald E. Knuth");
-        Article article1 = new Article("java17", "The Java Language Specification", "https://docs.oracle.com/javase/specs/jls/se17/html/index.html", "ORACLE");
+        Book book1 = new Book(
+                "knuth67",
+                "The Art of Computer Programming",
+                "d:/books/programming/tacp.ps",
+                1967,
+                "Donald E. Knuth"
+        );
+
+        Article article1 = new Article(
+                "java17",
+                "The Java Language Specification",
+                "https://docs.oracle.com/javase/specs/jls/se17/html/index.html",
+                "ORACLE"
+        );
 
         Catalog catalog = new Catalog("Catalog name..");
         catalog.add(book1);
