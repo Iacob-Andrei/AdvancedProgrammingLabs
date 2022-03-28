16ab11a9810a6a099ea445648211a5b8783326ff
Andrei Iacob <77151602+IacobAndrei15@users.noreply.github.com>
2022-03-28 20:06:32 +0300

    tiki

---
+++ Lab5/src/main/java/commands/InfoCommand.java
@@ -0,0 +1,44 @@
+package commands;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
+import org.apache.tika.sax.BodyContentHandler;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class InfoCommand extends Command {
+
+    public InfoCommand(){
+        this.name = "Info command";
+        this.description = "use Apache Tika in order to extract metadata from your catalog items.";
+    }
+
+    public void getInfo(){
+        System.out.println( name + " " + description );
+    }
+
+    public void info(String path){
+
+        try{
+            BodyContentHandler handler = new BodyContentHandler();
+            Metadata metadata = new Metadata();
+            FileInputStream inputstream = new FileInputStream(path);
+            ParseContext pcontext = new ParseContext();
+
+            HtmlParser htmlparser = new HtmlParser();
+            htmlparser.parse(inputstream, handler, metadata,pcontext);
+            System.out.println("Contents of the document:" + handler);
+            System.out.println("Metadata of the document:");
+            String[] metadataNames = metadata.names();
+
+            for(String name : metadataNames) {
+                System.out.println(name + ":   " + metadata.get(name));
+            }
+        } catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
--- Lab5/src/main/java/solution/Homework.java
+++ Lab5/src/main/java/solution/Homework.java
@@ -15,17 +15,28 @@ public class Homework {
                 "ORACLE"
         );
 
+        Book book1 = new Book(
+                "knuth67",
+                "The Art of Computer Programming",
+                "d:/books/programming/tacp.ps",
+                1967,
+                "Donald E. Knuth"
+        );
         Catalog catalog = new Catalog("Catalog name..");
 
         AddCommand commandAdd = new AddCommand();
         ListCommand commandList = new ListCommand();
         ViewCommand commandView = new ViewCommand();
         ReportCommand reportCommand = new ReportCommand();
+        //InfoCommand infoCommand = new InfoCommand();
 
         commandAdd.add(catalog, article1);
-        commandList.list(catalog);
+        commandAdd.add(catalog, book1);
+        //commandList.list(catalog);
         //commandView.view("target/catalog.json");
-        reportCommand.report(catalog);
 
+        reportCommand.report(catalog);
+        //infoCommand.info("target/template/report.html");
     }
+
 }
