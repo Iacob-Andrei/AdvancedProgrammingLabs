437d25c00ee31c270d9d7a847ef4569e970d9aa4
Andrei Iacob <77151602+IacobAndrei15@users.noreply.github.com>
2022-03-29 11:53:16 +0300

    initial

--- Lab5/src/main/java/solution/Homework.java
+++ Lab5/src/main/java/solution/Homework.java
@@ -32,11 +32,11 @@ public class Homework {
 
         commandAdd.add(catalog, article1);
         commandAdd.add(catalog, book1);
-        //commandList.list(catalog);
-        //commandView.view("target/catalog.json");
+        commandList.list(catalog);
+        commandView.view("target/catalog.json");
 
-        reportCommand.report(catalog);
-        infoCommand.info("target/template/report.html");
+        //reportCommand.report(catalog);
+        //infoCommand.info("target/template/report.html");
     }
 
 }
