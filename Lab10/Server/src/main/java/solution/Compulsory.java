84d8874e4cb0facf0311be3f6d63346c393e62dd
Andrei Iacob <77151602+IacobAndrei15@users.noreply.github.com>
2022-05-10 11:25:22 +0300

    server push

---
+++ Lab10/Server/src/main/java/solution/Compulsory.java
@@ -0,0 +1,11 @@
+package solution;
+
+import server.SimpleServer;
+
+import java.io.IOException;
+
+public class Compulsory {
+    public static void main(String[] args) throws IOException {
+        SimpleServer server = new SimpleServer();
+    }
+}
